c9e47f1d3e326de316a483b8373f704b33dd4872
ATom <aju_thomas@yahoo.com>
2019-06-28 19:53:38 -0400

    first commit

---
+++ springbootdemonew/src/main/java/com/thomas/springboot/demo/springbootdemonew/SpringbootdemonewApplication.java
@@ -0,0 +1,13 @@
+package com.thomas.springboot.demo.springbootdemonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemonewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemonewApplication.class, args);
+	}
+
+}
